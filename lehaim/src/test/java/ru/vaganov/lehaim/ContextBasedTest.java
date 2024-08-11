c25ec8a398ebd60324e63b271570743181ba8532
Ваганов Владислав <vaganovlad@gmail.com>
2024-08-11 14:44:27 +0500

    not working h2 tests

---
+++ lehaim/src/test/java/ru/vaganov/lehaim/ContextBasedTest.java
@@ -0,0 +1,13 @@
+package ru.vaganov.lehaim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextBasedTest {
+
+    @Test
+    void a(){
+
+    }
+}
